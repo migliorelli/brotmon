026ab60c9bc7eb649ab1f97bbc87c3d640e4247e
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-13 23:17:52 -0300

    created default middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,19 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname === "/api/battle/ws") {
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set("x-middleware-rewrite", pathname);
-
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  }
-
+export async function middleware(request: NextRequest) {
+  // return await updateSession(request);
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/api/battle/ws"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
