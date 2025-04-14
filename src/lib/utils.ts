ca2a63f2739395a7cd0448569125dced41dfcd49
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-13 23:17:12 -0300

    created clsx helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function tryCatch(fn: Function, error: string) {
+  try {
+    fn();
+    return true;
+  } catch {
+    console.error(error);
+    return false;
+  }
 }
