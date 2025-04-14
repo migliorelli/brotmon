a2d32e1d7843ed5035138d139bb7e91edd43a9c5
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-14 20:59:03 -0300

    feature: create executeTurn method

--- src/services/battle-service.ts
+++ src/services/battle-service.ts
@@ -210,9 +210,283 @@ export class BattleService {
     return { error: null };
   }
 
-  private async handleSwitch() {}
+  private async handleSwitchAction(trainer_id: string, brotmon_id: string) {}
 
-  private async executeTurn() {}
+  private async handleMoveAction(trainer_id: string, move_id: string) {}
+
+  private async handleSwitch(trainer_id: string, brotmon_id: string) {
+    const { data: trainerBrotmons, error: brotmonsError } = await this.supabase
+      .from("trainer_brotmons")
+      .select(
+        `
+          id, effects, current_hp,
+          base:brotmons!brotmon_id(
+            id, name, emoji, nature, hp, attack, defense, speed
+        )`,
+      )
+      .eq("trainer_id", trainer_id)
+      .neq("id", brotmon_id)
+      .gt("current_hp", 0)
+      .order("created_at", { ascending: true })
+      .limit(1);
+
+    if (brotmonsError) {
+      return { brotmon: null, error: brotmonsError.message };
+    } else if (trainerBrotmons.length === 0) {
+      return { brotmon: null, error: "No Brotmon alive" };
+    }
+
+    const brotmon = trainerBrotmons[0];
+    return { brotmon, error: null };
+  }
+
+  private async autoSwitchBrotmon(trainer_id: string) {
+    const { data: trainer, error: brotmonsError } = await this.supabase
+      .from("trainers")
+      .select(
+        `
+          id, username,
+          actions:battle_actions!trainer_id(id),
+          brotmons:trainer_brotmons!trainer_id(
+            id, current_hp,
+            base:brotmons!brotmon_id(
+              id, name
+            )
+        )`,
+      )
+      .eq("id", trainer_id)
+      .filter("brotmons.current_hp", "gt", 0)
+      .limit(1)
+      .order("brotmons.created_at", { ascending: true })
+      .single();
+
+    if (brotmonsError) {
+      return { error: brotmonsError.message };
+    } else if (trainer.brotmons.length === 0) {
+      return { error: "No Brotmon alive" };
+    }
+
+    return { error: null };
+  }
+
+  private async getMove(brotmon_id: string, move_id: string) {
+    const { data: moves, error: movesError } = await this.supabase
+      .from("brotmon_moves")
+      .select(
+        `
+          id, current_uses,
+          base:moves!move_id(
+            id, name, nature, type, power, accuracy, max_uses, always_crit, priority, effect
+          )
+        `,
+      )
+      .eq("id", move_id)
+      .eq("brotmon_id", brotmon_id)
+      .gt("current_uses", 0)
+      .limit(1);
+
+    if (movesError) {
+      return { move: null, error: movesError.message };
+    } else if (moves.length === 0) {
+      return { move: null, error: "Usage limit reached" };
+    }
+
+    const move = moves[0];
+    return { move, error: null };
+  }
+
+  private async executeTurn(
+    battleTurn: NonNullable<
+      Awaited<ReturnType<typeof this.getBattleTurn>>["battleTurn"]
+    >,
+  ) {
+    if (
+      battleTurn.host_action === null ||
+      battleTurn.guest_action === null ||
+      battleTurn.host_action.action === null ||
+      battleTurn.host_action.target_id === null ||
+      battleTurn.guest_action.action === null ||
+      battleTurn.guest_action.target_id === null
+    ) {
+      return {
+        result: null,
+        error: `Turn ${battleTurn.turn} is not ready yet`,
+      };
+    }
+
+    // store logs and send after all actions
+    const logs: string[] = [];
+
+    // copy
+    const actions = [
+      { ...battleTurn.host_action },
+      { ...battleTurn.guest_action },
+    ];
+
+    const hostSpeed = actions[0].brotmon.base.speed;
+    const guestSpeed = actions[1].brotmon.base.speed;
+
+    // get host speed multipliers (hsm)
+    const hsm = BattleUtils.getStatusMultiplier(
+      "speed",
+      actions[0].brotmon.effects as StatusEffect[],
+    );
+
+    // get guest speed multipliers (hsm)
+    const gsm = BattleUtils.getStatusMultiplier(
+      "speed",
+      actions[0].brotmon.effects as StatusEffect[],
+    );
+
+    // if hostSpeed < guestSpeed then Number(boolean) will be 1, the guest index
+    const first = Number(hostSpeed * hsm < guestSpeed * gsm);
+
+    const willSwitch = actions.map((a) => a.action === "SWITCH");
+
+    for (let i = 0; i < 2; i++) {
+      const ws = willSwitch[i];
+      if (!ws) continue;
+      const action = actions[i];
+      const action2 = actions[1 - i]; // [0,1] === [0,-1]
+
+      const { brotmon, error: switchError } = await this.handleSwitch(
+        action.trainer.id,
+        action.target_id!,
+      );
+
+      if (switchError === "No Brotmon alive") {
+        return {
+          result: {
+            finished: true,
+            winner_id: action2.trainer.id,
+            logs,
+          },
+          error: null,
+        };
+      } else if (switchError) {
+        return { result: null, error: switchError };
+      } else if (!brotmon) {
+        return {
+          result: null,
+          error: `Brotmon with ID ${action.target_id!} not found.`,
+        };
+      }
+
+      actions[i].brotmon = brotmon;
+    }
+
+    for (let i = 0; i < 2; i++) {
+      const attackerIndex = (first + i) % 2; // always return 0 | 1, starting with first
+      const attacker = actions[attackerIndex];
+
+      if (attacker.action === "SWITCH") continue;
+
+      const attackerBrotmon = attacker.brotmon;
+
+      const { brotmon: ieBrotmon, interrupt } =
+        BattleUtils.handleInterruptiveEffects(attackerBrotmon);
+      actions[attackerIndex].brotmon = ieBrotmon;
+
+      if (interrupt) {
+        if (interrupt.message) {
+          logs.push(interrupt.message);
+        }
+
+        continue;
+      }
+
+      const { move, error: moveError } = await this.getMove(
+        attackerBrotmon.id,
+        attacker.target_id!,
+      );
+
+      if (moveError === "Usage limit reached") {
+        return {
+          result: null,
+          error: `Move with ID ${attacker.target_id!} can'be used: limit of uses reached`,
+        };
+      } else if (moveError) {
+        return { result: null, error: moveError };
+      } else if (!move) {
+        return {
+          result: null,
+          error: `Move with ID ${attacker.target_id!} not found.`,
+        };
+      }
+
+      if (move.base.accuracy > Math.random()) {
+        logs.push(`${attackerBrotmon.base.name} missed ${move.base.name}!`);
+        continue;
+      }
+
+      const target = actions[1 - attackerIndex]; // [0,1] === [0,-1]
+      const targetBrotmon = target.brotmon;
+
+      if (move.base.type === "ATTACK") {
+        const attackerMultiplier = BattleUtils.getStatusMultiplier(
+          "attack",
+          attackerBrotmon.effects as StatusEffect[],
+        );
+
+        const defenseMultiplier = BattleUtils.getStatusMultiplier(
+          "defense",
+          targetBrotmon.effects as StatusEffect[],
+        );
+
+        const damage = BattleUtils.calculateDamage(
+          move.base.power,
+          attackerBrotmon.base.attack * attackerMultiplier,
+          targetBrotmon.base.defense * defenseMultiplier,
+          attackerBrotmon.base.nature.includes(move.base.nature),
+          move.base.nature as Nature,
+          targetBrotmon.base.nature as Nature[],
+        );
+
+        targetBrotmon.current_hp = Math.floor(
+          Math.max(0, targetBrotmon.current_hp - damage),
+        );
+
+        logs.push(
+          `${attackerBrotmon.base.name} used ${move.base.name} on ${targetBrotmon.base.name} and dealt ${damage} damage!`,
+        );
+      }
+
+      // apply or extend effect if it hits
+      if (
+        move.base.effect &&
+        (move.base.effect as StatusEffect).chance <= Math.random()
+      ) {
+        const effect = move.base.effect as StatusEffect;
+        BattleUtils.applyStatusEffect(targetBrotmon, effect, move.base.name);
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+      const action = actions[i];
+
+      if (action.brotmon.current_hp === 0) {
+        logs.push(`${action.brotmon.base.name} fainted!`);
+        const { error: asbError } = await this.autoSwitchBrotmon(
+          action.trainer.id,
+        );
+
+        if (asbError === "No Brotmon alive") {
+          return {
+            result: {
+              finished: true,
+              winner_id: actions[1 - i].trainer.id, // [0,1] === [0,-1],
+              logs,
+            },
+            error: null,
+          };
+        } else if (asbError) {
+          return { result: null, error: asbError };
+        }
+      }
+    }
+
+    return { result: { actions, logs }, error: null };
+  }
 
   public async performAction(
     battle_id: string,
@@ -259,6 +533,41 @@ export class BattleService {
 
       default:
         return { error: "Invalid action" };
+    }
+
+    const { battleTurn, error: battleTurnError } =
+      await this.getBattleTurn(battle_id);
+    if (battleTurnError !== null) return { error: battleTurnError };
+
+    const { result, error: executeError } = await this.executeTurn(battleTurn);
+
+    if (executeError) {
+      return { error: executeError };
+    } else if (!result) {
+      return { error: `Turn ${battleTurn.id} is not ready yet` };
+    }
+
+    for (let log of result.logs) {
+      const { error: logError } = await this.logMessage(
+        battle_id,
+        battleTurn.id,
+        log,
+      );
+
+      if (logError) {
+        console.error(logError);
+        return { error: logError };
+      }
+    }
+
+    if (result.finished) {
+      const { error } = await this.finishBattle(battle_id, result.winner_id);
+      if (error) {
+        return {
+          error,
+        };
+      }
+    } else if (result.actions) {
     }
 
     return { error: null };
