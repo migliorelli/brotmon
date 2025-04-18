969215f3e5a0a4eebe820dd8b91865aacb493339
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-18 00:00:30 -0300

    feat: implement battle join functionality and refactor related components; add error handling and improve user experience

---
+++ src/app/(app)/battle/join/[battle_id]/page.tsx
@@ -0,0 +1,20 @@
+import { createClient } from "@/lib/supabase/server";
+import { JoinBattlePage } from "@/pages/JoinBattlePage";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ battle_id: string }>;
+}) {
+  const { battle_id } = await params;
+
+  const supabase = await createClient();
+  const { data: brotmons, error } = await supabase.from("brotmons").select("*");
+
+  if (error) {
+    console.error("Error fetching Brotmons: ", error);
+    throw new Error(error.message);
+  }
+
+  return <JoinBattlePage brotmons={brotmons} battle_id={battle_id} />;
+}
---
+++ src/pages/JoinBattlePage.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { BrotmonItem } from "@/components/teambuilder/brotmon-selector";
+import {
+  Teambuilder,
+  TeambuilderData,
+} from "@/components/teambuilder/teambuilder";
+import { httpClient } from "@/lib/http-client";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+type JoinBattlePageProps = {
+  brotmons: BrotmonItem[];
+  battle_id: string;
+};
+
+export function JoinBattlePage({ brotmons, battle_id }: JoinBattlePageProps) {
+  const router = useRouter();
+
+  const handleJoinBattle = async (data: TeambuilderData) => {
+    try {
+      const response = await httpClient.post(`/battle/${battle_id}/join`, {
+        trainer: data,
+      });
+
+      if (response.status !== 200)
+        throw new AxiosError("Error joining battle", "400");
+
+      router.push(`/battle/${battle_id}/`);
+    } catch (e) {
+      const error = e as AxiosError<{ error: string }>;
+      toast.error(error.response?.data?.error ?? error.message);
+    }
+  };
+
+  return (
+    <main className="container mx-auto flex flex-col py-8">
+      <Teambuilder
+        brotmons={brotmons}
+        onSubmit={handleJoinBattle}
+        submitButtonText="🕹️ Join battle"
+      />
+    </main>
+  );
+}
