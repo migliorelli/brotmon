a904fbeef95fe1251f6f34917ae5ece4198677a7
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-20 18:16:41 -0300

    refactor:  reorganize structure to return if pathname is null

--- src/components/global/navbar/join-battle-title.tsx
+++ src/components/global/navbar/join-battle-title.tsx
@@ -4,14 +4,17 @@ import { usePathname } from "next/navigation";
 
 export function JoinBattleTitle() {
   const pathname = usePathname();
+
+  if (!pathname) return null;
+
   const isJoinBattlePage = pathname.startsWith("/battle/join/");
   const battle_id = pathname.split("/").at(-1);
 
   if (!isJoinBattlePage) return null;
 
   return (
-    <div className="my-auto text-center justify-self-center">
-      <h1 className="text-lg md:text-xl font-bold">Join Battle</h1>
+    <div className="my-auto justify-self-center text-center">
+      <h1 className="text-lg font-bold md:text-xl">Join Battle</h1>
       <p className="text-muted-foreground text-sm">Battle ID: {battle_id}</p>
     </div>
   );
