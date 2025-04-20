9bf86eedf1606baf2a0d623740869f7658c3a0fd
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-20 18:16:54 -0300

    refactor: add children types

--- src/components/global/navbar/navbar-link.tsx
+++ src/components/global/navbar/navbar-link.tsx
@@ -4,21 +4,16 @@ import { Button } from "@/components/ui/button";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
 
-export function NavbarLink({
-  href,
-  children,
-}: {
-  href: string;
-  children: any;
-}) {
+export function NavbarLink({ href, children }: { href: string; children: ReactNode }) {
   const pathname = usePathname();
 
   return (
     <Button
       variant="outline"
       className={clsx(
-        "hover:bg-secondary border-0 dark:bg-transparent shadow-none",
+        "hover:bg-secondary border-0 shadow-none dark:bg-transparent",
         pathname === href && "bg-secondary dark:bg-secondary",
       )}
     >
