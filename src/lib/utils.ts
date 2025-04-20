144875471f841b17c22e164862cfc93f00390327
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-04-20 18:15:31 -0300

    refactor: remove trycatch function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function tryCatch(fn: Function, error: string) {
-  try {
-    fn();
-    return true;
-  } catch {
-    console.error(error);
-    return false;
-  }
-}
